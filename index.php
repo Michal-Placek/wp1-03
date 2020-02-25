aed3354a133be66b37009df17298ad5108eabea1
Michal-Placek <michal.placek@student.ossp.cz>
2020-02-25 08:42:48 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+        $a=10;
+        $b=5;
+?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ <p>
+
+
+<p>Součet: <?= $a> + <?= $b?> = <?= $a + $b ?> </p>
+</body>
+</html>
